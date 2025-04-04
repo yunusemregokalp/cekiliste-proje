60c89034a0374dd2afd9c8836b34fc2a23406446
yunusemregokalp <157820856+yunusemregokalp@users.noreply.github.com>
2025-04-04 18:52:03 +0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/cekiliste-proje/'  // GitHub Pages için repo adıyla uyumlu base path
-})
