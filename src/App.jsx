741f674e032ceb9825b70e1e27a4faeb17620dff
yunusemregokalp <157820856+yunusemregokalp@users.noreply.github.com>
2025-04-04 18:52:19 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Çeki Listesi ve Koli Etiket Sistemi</h1>
+      <p>Buraya formlar ve veriler gelecek.</p>
+    </div>
+  );
+}
+
+export default App;
